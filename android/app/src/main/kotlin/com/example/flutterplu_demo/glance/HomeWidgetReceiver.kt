22f824e623d2ba8bae18f28029b628061aafd95f
damo <damo0821@163.com>
2025-02-25 22:21:11 +0800

    恢复数据

--- android/app/src/main/kotlin/com/example/flutterplu_demo/glance/HomeWidgetReceiver.kt
+++ android/app/src/main/kotlin/com/example/flutterplu_demo/glance/HomeWidgetReceiver.kt
@@ -2,6 +2,9 @@ package com.example.flutterplu_demo.glance
 
 import HomeWidgetGlanceWidgetReceiver
 
+
 class HomeWidgetReceiver : HomeWidgetGlanceWidgetReceiver<AppWidget>() {
     override val glanceAppWidget = AppWidget()
+
+
 }
